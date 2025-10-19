cef2b7a7a7a7a6a4f9ddd6346c21eb2c2ee7f6b6
Pedro27-J <pedro.l.jimenez@unl.edu.ec>
2025-10-18 21:03:18 -0500

    Agrego de codigo de Accion

--- src/main/java/modelo/Accion.java
+++ src/main/java/modelo/Accion.java
@@ -1,4 +1,67 @@
 package modelo;
 
+import java.time.LocalDateTime;
+
 public class Accion {
+
+    private String tipo;
+    private String descripcion;
+    private LocalDateTime fechahora;
+    private String object;
+
+    //Uso del constructor
+
+    public Accion(String tipo, String descripcion, LocalDateTime fechahora, String object) {
+        this.tipo = tipo;
+        this.descripcion = descripcion;
+        this.fechahora = fechahora;
+        this.object = object;
+    }
+
+    //Uso de un Getter and Setter
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public LocalDateTime getFechahora() {
+        return fechahora;
+    }
+
+    public void setFechahora(LocalDateTime fechahora) {
+        this.fechahora = fechahora;
+    }
+
+    public String getObject() {
+        return object;
+    }
+
+    public void setObject(String object) {
+        this.object = object;
+    }
+
+    //Uso de un toString
+
+    @Override
+    public String toString() {
+        return "Accion{" +
+                "tipo='" + tipo + '\'' +
+                ", descripcion='" + descripcion + '\'' +
+                ", fechahora=" + fechahora +
+                ", object='" + object + '\'' +
+                '}';
+    }
 }
+
